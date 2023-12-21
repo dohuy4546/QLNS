a93a66c251bd8b754027f2670424360bb5813b95
Đỗ Gia Huy <94332741+dohuy4546@users.noreply.github.com>
2023-12-21 18:34:25 +0700

    fix admin models

--- app/admin.py
+++ app/admin.py
@@ -9,12 +9,32 @@ admin = Admin(app=app, name='QUẢN LÝ NHÀ SÁCH', template_mode='bootstrap4')
 
 
 class SachView(ModelView):
+    column_display_pk = True
+    can_create = True
     column_list = ['id', 'tensach', 'gia', 'anhbia', 'soluongtonkho']
     can_export = True
     column_searchable_list = ['tensach']
     column_filters = ['gia', 'tensach']
-    column_editable_list = ['tensach', 'gia', 'anhbia', 'soluongtonkho']
+    column_editable_list = ['id', 'tensach', 'gia', 'anhbia', 'soluongtonkho']
+    details_modal = True
+    edit_modal = True
+    form_columns = ['id', 'tensach', 'gia', 'anhbia', 'soluongtonkho']
+
+class TheLoaiView(ModelView):
+    column_list = ['id', 'tentheloai']
+    can_export = True
+    column_searchable_list = ['tentheloai']
+    column_filters = ['tentheloai']
+    column_editable_list = ['tentheloai']
     details_modal = True
     edit_modal = True
 
+class Sach_TheLoaiView(ModelView):
+    column_list = ['id', 'sach_id', 'theloai_id']
+    form_columns = ['sachs', 'theloais']
+    can_export = True
+
+
 admin.add_view(SachView(Sach, db.session))
+admin.add_view(TheLoaiView(TheLoai, db.session))
+admin.add_view(Sach_TheLoaiView(Sach_TheLoai, db.session))
--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from app import db, app
 from flask_login import UserMixin
 import enum
 
+
 class BaseModel(db.Model):
     __abstract__ = True
 
@@ -15,22 +16,26 @@ class LoaiTaiKhoan(enum.Enum):
     KHACHHANG = 2
     ADMIN = 3
 
+
 class TaiKhoan(db.Model, UserMixin):
     __abstract__ = True
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     username = Column(String(50), nullable=False, unique=True)
     password = Column(String(50), nullable=False)
-    avatar = Column(String(100), default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+    avatar = Column(String(100),
+                    default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
     user_role = Column(Enum(LoaiTaiKhoan))
 
     def __str__(self):
         self.username
+
 
 class TaiKhoanKhachHang(TaiKhoan):
     email = Column(String(100), primary_key=True)
     diachi = Column(String(1000), nullable=True)
     sdt = Column(String(10), nullable=True)
     hoten = Column(String(100), nullable=True)
+
 
 class NhanVien(BaseModel):
     CCCD = Column(String(20), nullable=False, unique=True)
@@ -41,31 +46,44 @@ class NhanVien(BaseModel):
     def __str__(self):
         return self.hoten
 
+
 class TaiKhoanNhanVien(TaiKhoan):
     nhanvien_id = Column(Integer, ForeignKey(NhanVien.id), unique=True, nullable=False)
     nhanvien = relationship("NhanVien", back_populates="taikhoannhanvien", uselist=False)
 
 
-
 class TheLoai(BaseModel):
+    __tablename__ = 'theloai'
     tentheloai = Column(String(100), nullable=False, unique=True)
-
+    sach_theloai = relationship('Sach_TheLoai', backref='theloai', lazy=True)
     def __str__(self):
         return self.tentheloai
 
+
 class Sach(db.Model):
-    id = Column(String(10), primary_key=True)
+    __tablename__='sach'
+    id = Column(String(10), primary_key=True, nullable=False)
     tensach = Column(String(100), nullable=False, unique=True)
     gia = Column(Float, default=0)
     anhbia = Column(String(100))
     soluongtonkho = Column(Integer)
     sach_theloai = relationship('Sach_TheLoai', backref='sach', lazy=True)
+
     def __str__(self):
         return self.tensach
+
+
 class Sach_TheLoai(BaseModel):
+    __tablename__='sach_theloai'
     sach_id = Column(String(10), ForeignKey(Sach.id), nullable=False)
     theloai_id = Column(Integer, ForeignKey(TheLoai.id), nullable=False)
+    sachs = relationship('Sach', backref='sach_theloai1', lazy=True)
+    theloais = relationship('TheLoai', backref='sach_theloai2', lazy=True)
+
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        # sach1 = Sach(id='VH1234', tensach='Van hoc truyen thong Viet Nam', gia=100000, soluongtonkho=200)
+        # db.session.add(sach1)
+        # db.session.commit()
